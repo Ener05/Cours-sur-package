6398ad0b3f052476b086cbcc1ea57c52b95a9dd9
Ener05 <62993774+Ener05@users.noreply.github.com>
2020-05-20 21:55:20 +0000

    creation de api

---
+++ database/seeds/StudentsTableSeed.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StudentsTableSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $marks = [
+            'name' => 'rene',
+            'course' => 'php'
+        ]
+    }
+}
